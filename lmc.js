7222db8e320fce6bf5e94313aba7687cf0bcde46
Antti <antti@Anttis-MacBook-Pro.local>
2018-01-07 14:51:29 +0200

    added loadInstruction and execute functions

--- lmc.js
+++ lmc.js
@@ -8,8 +8,8 @@ const computer = {
 	negativeFlag: false,
 	endOfProgram: false,
 
-	/* Resets computer to starting state */
 	reset() {
+		/* Resets computer to starting state */
 		this.memory.fill(0);
 		this.programCounter = 0;
 		this.cycleCount = 0;
@@ -22,6 +22,7 @@ const computer = {
 
 
 	add(slot) {
+		/* Takes slot number as an input and adds value in that memory slot to the accumulator */
 		const tmp = this.accumulator + this.memory[slot];
 		try {
 			if (tmp > 999) throw "overflow";
@@ -35,6 +36,7 @@ const computer = {
 	},
 
 	subtract(slot) {
+		/* Substract value in memory slot pointed by input from the accumulator */
 		const tmp = this.accumulator - this.memory[slot];
 		if (tmp < 0) {
 			this.negativeFlag = true;
@@ -115,4 +117,57 @@ const computer = {
 
 		this.memory[slot] = value;
 	},
+
+	loadInstructions(instructionArray) {
+		/* Loads an array of instrucions (3 digit integers) to memory. Intructions after HALT (000) are considered to be DATA instructions and the values are loaded to  next free memory slot. */
+		try {
+			if (instructionArray.constructor === Array) throw "invalid instructionArray";
+			if (instructionArray.length > 100) throw "too many instructions";
+		}
+		catch(err) {
+			console.log(err);
+		}
+		instructionArray.forEach(this.data);
+	}
+
+	execute(value) {
+		try {
+			if (!Number.isInteger(value)) throw "invalid value";
+			if (value > 999) throw "value out of upper bound";
+			if (value < 0) throw "value out of lower bound";
+		}
+		catch(err) {
+			console.log(err);
+		}
+
+		const operation = value / 100; 
+		const slot = value % 100;
+
+		if (value == 0) {
+			
+		} else if (operation == 1) {
+			this.add(slot);
+		} else if (operation == 2) {
+			this.subtract(slot);
+		} else if (operation == 3) {
+			this.store(slot);
+		} else if (operation == 5) {
+			this.load(slot);
+		} else if (operation == 6) {
+			this.branch(slot);		
+		} else if (operation == 7) {
+			this.branchIfZero(slot);
+		} else if (operation == 8) {
+			this.branchIfPositive(slot);
+		} else if (value == 901) {
+			this.input();
+		} else if (value == 902) {
+			this.output();
+		} else {
+			console.log("invalid operation");
+		}
+	},
+	
+
+
 };
